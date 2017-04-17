c0f9907efd9c91795ea99cd816d296e625c2515d
Javier <javierbelbruno@gmail.com>
2017-04-16 22:19:08 -0300

    probandoGItHUb

--- TP3SIM/Poisson.Designer.cs
+++ TP3SIM/Poisson.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Probando gitHUB";
             // 
             // Poisson
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(426, 325);
+            this.Controls.Add(this.label1);
             this.Name = "Poisson";
             this.Text = "Poisson";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
